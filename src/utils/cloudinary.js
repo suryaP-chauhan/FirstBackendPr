48a2daa670060cb871304e6a2315e1c26f5a5eab
surya pratap singh chauhan <suryanshrajachauhan@gmail.com>
2024-08-11 18:27:17 +0530

    updated

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,7 +6,8 @@ import fs from 'fs';
     cloudinary.config({ 
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
         api_key: process.env.CLOUDINARY_API_KEY, 
-        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View Credentials' below to copy your API secret
+        api_secret: process.env.CLOUDINARY_API_SECRET
+        // Click 'View Credentials' below to copy your API secret
     });
     
     const uploadOnCloudinary = async (localFilePath) => {
@@ -17,6 +18,7 @@ import fs from 'fs';
                 resource_type: "auto"
             })
             // file has been uploaded successfully
+            fs.unlinkSync(localFilePath)
             return response
         } catch (error) {
             fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
